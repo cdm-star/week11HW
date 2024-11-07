2c2f09b90e1b1919b13b8e7f0755e0df6f0fcaa7
cdm-star <colin0503@icloud.com>
2024-11-07 09:38:17 -0500

    still need to find help to finish

--- week11longGame/week11longGame/Program.cs
+++ week11longGame/week11longGame/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace week11longGame
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace week11longGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a name");
+            string userChoice=Console.ReadLine();
+
+            int score = 0;
+
+            while (true)
+            {
+                if (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    score++;
+                 
+                }
+                else if(Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                 
+                    break;
+                }
+            }
         }
     }
 }
